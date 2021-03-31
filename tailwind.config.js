7a1348c6e540b666fd1fe7f69f97635187336a86
AmamDewan <cybercrafttech@gmail.com>
2021-04-01 04:27:37 +0600

    v5

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#EF4E22',
+        'second': '#4C4C4E'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
